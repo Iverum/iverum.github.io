dd3d19c8b3b04007bea8de58b5221f121baea09f
Blake Hair <blake.hair@gmail.com>
2015-12-06 16:20:54 -0500

    Added descriptions for most projects

--- data/projects.js
+++ data/projects.js
@@ -14,11 +14,13 @@ module.exports =
 }, {
   "title": "Content Pipeline Tooling",
   "blurb": "Scripting/content pipeline tools for in-house game engine.",
-  "image": "http://placehold.it/350x250"
+  "image": "http://placehold.it/350x250",
+  "description": "A tooling system for an in-house game engine at the Digital Corps. Used a web-based front-end to allow designers and writers to input scripts and assets. An API allowed the scripts and assets to be exported into formats the game engine could read. I implemented a set of bash scripts that would poll the API daily and build the new assets into the project, committing the build if it succeeded. Additional scripts would allow developers more control over the importation of assets."
 }, {
   "title": "BSU Map App",
   "blurb": "Mobile app to guide students and visitors on Ball State's campus.",
   "image": "http://placehold.it/350x250",
+  "description": "Explore every corner of Ball State University in Muncie, Indiana, and navigate the college experience from the palm of your hand. BSU's Campus Map is exactly what every student needs to manage time wisely. That means no more getting lost, missing meetings, or last minute struggles to park your car. And it's not just about getting from place to place; you can find hotspots, search for buildings, departments and more.",
   "links": [{
     "text": "App Store",
     "href": "https://itunes.apple.com/us/app/ball-state-university-campus/id353148218?mt=8"
@@ -30,6 +32,7 @@ module.exports =
   "title": "BSU Rec App",
   "blurb": "Usage statistics for Ball State University's recreation facilities",
   "image": "http://placehold.it/350x250",
+  "description": "The Ball State University Recreation Services App helps students, faculty, staff, alumni and guests identify if their favorite recreation facilities are busy. Updated in real time, there's no need to waste time heading to the gym only to find it too crowded. The Rec app helps you easily identify how busy all areas of the Student Recreation & Wellness Center, Ball Gymnasium, and Lewellen Pool are. Go when you feel comfortable, knowing you will get the most out of your workout.",
   "links": [{
     "text": "App Store",
     "href": "https://itunes.apple.com/us/app/bsu-recreation-services/id498386770?mt=8"
@@ -40,11 +43,13 @@ module.exports =
 }, {
   "title": "Circuit Soaker",
   "blurb": "Competitive multiplayer game for Ball State University's Multi-user Touch Billboard and iOS devices.",
-  "image": "http://placehold.it/350x250"
+  "image": "http://placehold.it/350x250",
+  "description": "Using the large multi-user touch board outside of the Digital Corps office as a host, we built a multiplayer line defense game. The defending player begins a game on the touch board defending their base from computer controlled robots attempting to destroy the power station. Passersby can join in, taking control of the attacking robots, on their iOS devices."
 }, {
   "title": "Project Devourer",
   "blurb": "On-set rough cuts using footage imported to an iPad.",
-  "image": "http://placehold.it/350x250"
+  "image": "http://placehold.it/350x250",
+  "description": "An app for the quick editing of films for rough cuts on set. Shoot footage from your iPad or import footage to the iPad to use simple non-linear editing tools to create daily cuts. Export the cuts to Final Cut Pro or Adobe Premier for more refined editing later."
 }, {
   "title": "Exceptional Me",
   "blurb": "Time management calendar app for twice-exceptional primary school students on iPads.",
