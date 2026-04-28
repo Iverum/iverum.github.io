1c7b023fe71038ba1352810d19637b897c974f2d
Blake Hair <blakehair@gmail.com>
2026-04-28 16:07:27 -0400

    add first blog post

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,10 @@
-module.exports = function(eleventyConfig) {
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/");
   eleventyConfig.addPassthroughCopy("media/");
+
+  eleventyConfig.addFilter("dateISO", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
 };
