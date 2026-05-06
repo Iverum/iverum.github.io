094019608350799b84a1ddf46354c509d386ced5
Blake Hair <blakehair@gmail.com>
2026-05-05 20:37:35 -0400

    add footnote support to blog posts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,11 @@
 const { DateTime } = require("luxon");
+const markdownItFootnote = require("markdown-it-footnote");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/");
   eleventyConfig.addPassthroughCopy("media/");
+
+  eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItFootnote));
 
   eleventyConfig.addFilter("dateISO", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
