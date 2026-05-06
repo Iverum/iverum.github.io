1b8c6ca1adf70d2f46703371bdded67c7914bc82
Blake Hair <blakehair@gmail.com>
2026-05-05 21:47:47 -0400

    adds atom rss feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 const { DateTime } = require("luxon");
 const markdownItFootnote = require("markdown-it-footnote");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles/");
@@ -9,5 +10,24 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("dateISO", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
+
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "posts",
+      limit: 10,
+    },
+    metadata: {
+      language: "en",
+      title: "Blake Hair",
+      subtitle: "Personal blog of Blake Hair",
+      base: "https://blakehair.com/",
+      author: {
+        name: "Blake Hair",
+        email: "", // Optional
+      }
+    }
   });
 };
